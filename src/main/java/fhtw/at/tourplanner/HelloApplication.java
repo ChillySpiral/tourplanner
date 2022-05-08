88d10017efec3db360f9364b0a551ce52acb0b29
ChillySpiral <81975828+ChillySpiral@users.noreply.github.com>
2022-05-08 12:23:47 +0200

    Finished basis for Layout

--- src/main/java/fhtw/at/tourplanner/HelloApplication.java
+++ src/main/java/fhtw/at/tourplanner/HelloApplication.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("test123.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1200, 600);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("homeView.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        stage.setTitle("Tourplanner");
         stage.setScene(scene);
         stage.show();
     }
