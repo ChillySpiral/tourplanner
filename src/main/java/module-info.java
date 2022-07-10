5f6025d256e98d46feb671e42a5b313989ecc063
ChillySpiral <81975828+ChillySpiral@users.noreply.github.com>
2022-07-10 20:37:49 +0200

    fix

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -28,6 +28,5 @@ module fhtw.at.tourplanner {
     opens fhtw.at.tourplanner.PL.view.dialog to javafx.fxml;
     exports fhtw.at.tourplanner.DAL.model.export;
     exports fhtw.at.tourplanner.DAL.model.enums;
-    exports fhtw.at.tourplanner.DebugRunner;
     exports fhtw.at.tourplanner.DAL.model.weatherModel;
 }
